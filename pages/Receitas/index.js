5c743e258801c02ad879f387c454e17b15d1a3a7
Dracods <39202981+Dracods@users.noreply.github.com>
2021-02-16 10:01:14 -0300

    Pagina Sobre e de Receitas adicionadas

---
+++ pages/Receitas/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Image} from 'react-native';
+
+export default function Receitas(props){
+    return <View style={styles.container}>
+    
+            </View>
+}
+const styles = ({
+    container:{
+
+    }
+})
---
+++ pages/Sobre/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Image} from 'react-native';
+
+export default function Sobre(props){
+    return <View style={styles.container}>
+    
+            </View>
+}
+const styles = ({
+    container:{
+
+    }
+})
